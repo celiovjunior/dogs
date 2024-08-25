ef521951912aab28445b2e7c5212cad4e8713e10
Celio Junior <celio_cjr@hotmail.com>
2024-08-25 20:36:07 +0930

    feat: created function to POST and GET using base url, salving token in localStorage, getting user by token function.

--- src/components/login/login-form.jsx
+++ src/components/login/login-form.jsx
@@ -2,28 +2,44 @@ import { Link } from "react-router-dom";
 import { FormInput } from "../form/form-input";
 import { FormButton } from "../form/form-button";
 import useForm from "../../hooks/useForm";
+import { TOKEN_POST, USER_GET } from "../../api";
+import { useEffect } from "react";
 
 export function LoginForm() {
     const username = useForm('email')
     const password = useForm()
 
-    function handleSubmit(event) {
+    useEffect(() => {
+        const token = window.localStorage.getItem('token');
+        if (token) {
+            getUser(token);
+        } else {
+            console.log('No token found');
+        }
+    }, [])
+
+    async function getUser(token) {
+        const { url, options } = USER_GET(token)
+        const response = await fetch(url, options)
+        const json = await response.json();
+        console.log(json);
+    }
+
+    async function handleSubmit(event) {
         event.preventDefault();
 
         if (username.validate() && password.validate()) {
-            fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(),
-            })
-            .then((response) => {console.log(response)
-                return response.json();
-            })
-            .then((json) => {
-                console.log(json);
-            })
+            const {url, options} = TOKEN_POST({
+                username: username.value,
+                password: password.value
+            });
+
+            const response = await fetch(url, options)
+            const json = await response.json();
+
+            window.localStorage.setItem('token', json.token);
+
+            getUser(json.token);
         }
     }
 
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 const types = {
     email: {
         regex: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-        message: "Please enter a valid email address",
+        message: "Please enter a valid email address.",
     }
 }
 
@@ -15,15 +15,13 @@ export default function useForm(type) {
         if (type === false) return true
 
         if (value.length === 0) {
-            setError('Fill a value');
+            setError('Fill a value.');
             return false;
         } else if (types[type] && !types[type].regex.test(value)) {
             setError(types[type].message)
-
             return false;
         } else {
             setError(null);
-            
             return true;
         }
     }
